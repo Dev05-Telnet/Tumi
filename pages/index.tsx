1c6ffe52c11f66dc8a9161682d2cdad5ae9e8b7f
Dev05-Telnet <dev05.telnet@gmail.com>
2021-10-20 18:18:55 +0530

    Data fetched from Pirsmic and showed in front

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import commerce from '@lib/api/commerce'
 import { Layout } from '@components/common'
 import { ProductCard } from '@components/product'
 import IntroSection from '@components/partials/home/intro-section'
+import IntroSectionTwo from '@components/partials/home/intro-section-two'
+import SliceZone from '@components/prismic/SliceZone'
 import ProductCollection from '@components/partials/home/product-collection'
 import { Grid, Marquee, Hero } from '@components/ui'
 import BannerSection from '@components/partials/home/banner-section';
@@ -9,6 +11,8 @@ import BannerSectionTwo from '@components/partials/home/banner-section-two';
 import FeaturedCollection from '@components/partials/home/featured-collection';
 import TopRatedCollection from '@components/partials/home/top-rated-collection';
 import type { GetStaticPropsContext, InferGetStaticPropsType } from 'next'
+
+import { Client } from '@utils/prismicHelpers'
 
 export async function getStaticProps({
   preview,
@@ -29,12 +33,18 @@ export async function getStaticProps({
   const { pages } = await pagesPromise
   const { categories, brands } = await siteInfoPromise
 
+  const client = Client()
+
+  // const doc = await client.getSingle('american_tourister', {}) || {}
+  // get homepage of american tourister by ID
+  const homePage = await client.getByID('YW6FtBIAAJvfAdDu', {}) || {}
   return {
     props: {
       products,
       categories,
       brands,
       pages,
+      homePage,
     },
     revalidate: 60,
   }
@@ -43,6 +53,7 @@ export async function getStaticProps({
 var loading = false;
 export default function Home({
   products,
+  homePage,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
@@ -53,6 +64,10 @@ export default function Home({
         <div className="container">
 
           <IntroSection />
+
+          <IntroSectionTwo banners={homePage.data.homepage_banner}/>
+
+          <SliceZone sliceZone={homePage.data.body} />
 
           <ProductCollection products={products} />
 
