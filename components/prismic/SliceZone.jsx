eaf3e22d10f85aef63f94f07912fcb9b1ae66030
Dev05-Telnet <dev05.telnet@gmail.com>
2021-10-21 16:59:41 +0530

    new Slice : ImageWithDesc

--- components/prismic/SliceZone.jsx
+++ components/prismic/SliceZone.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import CardsCarousel from './CardsCarousel'
 import CallToAction from './CallToAction'
-
+import ImageWithDesc from './ImageWithDesc'
 const SliceZone = ({ sliceZone }) => (
   <div className="container">
     {sliceZone.map((slice, index) => {
@@ -10,6 +10,8 @@ const SliceZone = ({ sliceZone }) => (
           return <CardsCarousel slice={slice} key={`slice-${index}`} />
         case ('call_to_action'):
           return <CallToAction slice={slice} key={`slice-${index}`} />
+        case ('image_with_description'):
+          return <ImageWithDesc slice={slice} key={`slice-${index}`} />
         default:
           return null
       }
